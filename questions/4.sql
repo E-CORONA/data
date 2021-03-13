85013a38e8dfe4ae366db8e3bc233ade72d2e931
180107266 <55758989+180107266@users.noreply.github.com>
2021-03-14 00:10:30 +0600

    Update 4.sql

--- questions/4.sql
+++ questions/4.sql
@@ -1,5 +1,5 @@
 /*How can a user be added to consultation with a doctor? (for example Marat with id 10001)*/
 
 INSERT INTO online_consultation (consultation_id, user_id, doctor_id, age, consultation_date, clinics, city, phone)
-VALUES (9008, 10001, 10003, 18, '19-Dec-20', 'SBS MED Clinic', 'Almaty', 87023647661);
+VALUES (9008, 10001, 10003, 18, '19-Mar-21', 'Medical Center "RADA" on Toraigyrov', 'Almaty', 87023647661);
 
