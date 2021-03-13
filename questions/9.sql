e91a04cb5ea77499a6a0a0d0a897614ef0e3aa3c
180107266 <55758989+180107266@users.noreply.github.com>
2021-03-14 00:26:49 +0600

    Create 9.sql

---
+++ questions/9.sql
@@ -0,0 +1,4 @@
+/*How can we insert(register) new users?*/
+
+INSERT INTO eco_users (user_id, first_name, last_name, city, contact_number, email, password, profile_image)
+VALUES (10172, 'Zhanel', 'Oralova', 'Almaty', 87083647661, 'pojalelbynonet@gmail.com', 'ddddd', '';);
