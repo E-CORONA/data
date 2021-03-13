0b1fd3d616feba8b95f0286bda5a7ea50049601c
180107266 <55758989+180107266@users.noreply.github.com>
2021-03-13 23:44:08 +0600

    Create 2.sql

---
+++ questions/2.sql
@@ -0,0 +1,4 @@
+/*2. What if a user wants to authorize from a non-email format?
+Regex for checking email format:*/
+
+ALTER TABLE users add constraint email_check CHECK(regexp_like(email, '.+@.+\..+'));
