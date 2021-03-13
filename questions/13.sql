ac7acea5a3791f9c3cbc516b332a0ae6280dc528
180107266 <55758989+180107266@users.noreply.github.com>
2021-03-14 01:20:04 +0600

    Create 13.sql

---
+++ questions/13.sql
@@ -0,0 +1,3 @@
+/* 13. How to get info about of medical center near your home (name, full address, contacts) knowing only the name of street and city?  
+
+SELECT mcenter_name, address, contacts FROM medical_center WHERE address LIKE '%Manasa%'; 
