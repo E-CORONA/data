46b06e50d097a150e7026d0da7b875c11383303a
180107266 <55758989+180107266@users.noreply.github.com>
2021-03-14 00:09:19 +0600

    Create 5.sql

---
+++ questions/5.sql
@@ -0,0 +1,4 @@
+/* 5.How can a user be added to PCR testing in a country's medical centers? (for example Marat with user id 10001 to clinics Medical Partners Korea Clinic LLP) */
+
+INSERT INTO online_pcr (pcr_id, center_id, user_id, first_name, last_name, city, punkt, pcr_date)
+VALUES (9016, 5002, 10001, 'Marat','Daniyarbekov', 'Almaty', 'Medical Partners Korea Clinic LLP', '19-Mar-21');
